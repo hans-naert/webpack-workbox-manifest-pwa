5f50480f59e33527ade9a59f0174e464e1e9dd83
Hans Naert <hans.naert@vives.be>
2023-10-04 22:16:12 +0200

    adapted port of Express server to deploy on Azure

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const webPush = require('web-push')
 const app = express()
-const port = 3000
+const port = 3000 | process.env.PORT
 let subscriptions = []
 
 app.use(express.static('dist'))
